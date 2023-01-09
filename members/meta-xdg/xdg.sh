8d98908595b10465b8951a48f5ac400e049a8ee3
Boyang Yan <yanboyang713@gmail.com>
2023-01-09 17:25:37 -0500

    backup zsh

--- members/meta-xdg/xdg.sh
+++ members/meta-xdg/xdg.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_USER_HOME="$HOME"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_DATA_DIRS="/usr/local/share:/usr/share"
+export XDG_CONFIG_DIRS="/etc/xdg"
